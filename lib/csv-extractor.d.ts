54f3b5dc575ecdd8ca0cfbd46f7e990e06d82035
Arayor <aaron.salasrodriguez@gmail.com>
2023-11-12 04:24:14 -0400

    first build

---
+++ lib/csv-extractor.d.ts
@@ -0,0 +1,8 @@
+import { ParseConfig } from "papaparse";
+declare type PapaString = string | null | number;
+export declare function processFile(file: File | any, parseConfig?: ParseConfig): Promise<any[] | undefined>;
+export declare function processXlsxFile(file: File): Promise<any[]>;
+export declare function processCsvFile(file: File | any, parseConfig?: ParseConfig): Promise<any[] | undefined>;
+export declare function getCsvData(file: File | any, inputConfig?: ParseConfig): Promise<PapaString[][]>;
+export declare function processCsvData(data: PapaString[][]): any[];
+export {};
